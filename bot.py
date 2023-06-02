1d20ab0a7044f9737413938858cae0204b95b2fc
Alex <alexey85p@yandex.ru>
2023-06-02 15:24:55 +0300

    MoscowZoo base commit

---
+++ bot.py
@@ -0,0 +1,13 @@
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['help', 'start'])
+def start_help(message: telebot.types.Message):
+    text = 'Вместо хелпы и приветствия,\n\
+Это болванка для бота Московского зоопарка\n\
+Нам надо дружно сделать из него конфетку)))'
+    bot.reply_to(message, text)
+
+bot.polling()
