d85a40bdce588f128ab8d49ad871ecdf9704ae20
Alex <alexey85p@yandex.ru>
2023-06-02 15:37:18 +0300

    commit1

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['help', 'start'])
 def start_help(message: telebot.types.Message):
-    text = 'Вместо хелпы и приветствия,\n\
+    text = 'Вместо хелпы и приветствия:\n\
 Это болванка для бота Московского зоопарка\n\
 Нам надо дружно сделать из него конфетку)))'
     bot.reply_to(message, text)
